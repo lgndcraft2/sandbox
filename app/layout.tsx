53ba16a30a9cc6b12857665d2f9c5463451a8644
Lloydgapton <lloydgapton123@gmail.com>
2025-12-17 06:17:54 -0800

    adjusted the sooner position

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Sonner position="top-right" />
+        <Sonner />
         <Toaster />
         <AuthProvider>
             {children}
